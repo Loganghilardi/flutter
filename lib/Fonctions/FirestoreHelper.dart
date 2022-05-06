184454dbf6514b4aadcaa2bf8dad57cce2e29afc
lgh <logan.ghilardi@netways.fr>
2022-05-06 11:37:35 +0200

    Helper

--- lib/Fonctions/FirestoreHelper.dart
+++ lib/Fonctions/FirestoreHelper.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:async';
 import 'dart:typed_data';
 
@@ -7,66 +5,105 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firstapplicationeisi/model/MyProfil.dart';
+import 'package:firstapplicationeisi/model/Utilisateur.dart';
 
 class FirestoreHelper {
   //Attributs
   final auth = FirebaseAuth.instance;
   final fireUser = FirebaseFirestore.instance.collection("Users");
   final storage = FirebaseStorage.instance;
-
+  final fire_message = FirebaseFirestore.instance.collection("Message");
+  final fire_conversation =
+      FirebaseFirestore.instance.collection('Conversations');
 
   //Méthodes
 
   // fonction pour s'inscrire
-  Future Inscription(String prenom , String nom , String mail , String password) async {
-    UserCredential result = await auth.createUserWithEmailAndPassword(email: mail, password: password);
+  Future Inscription(
+      String prenom, String nom, String mail, String password) async {
+    UserCredential result = await auth.createUserWithEmailAndPassword(
+        email: mail, password: password);
     String uid = result.user!.uid;
-    Map<String,dynamic> map = {
-      "PRENOM" : prenom,
-      "NOM" : nom,
+    Map<String, dynamic> map = {
+      "PRENOM": prenom,
+      "NOM": nom,
       "MAIL": mail,
     };
     addUser(uid, map);
-
   }
 
-
   //Fonction pour se conneter
-  Future <MyProfil> Connexion( String mail , String password) async {
-    UserCredential result = await auth.signInWithEmailAndPassword(email: mail, password: password);
+  Future<MyProfil> Connexion(String mail, String password) async {
+    UserCredential result =
+        await auth.signInWithEmailAndPassword(email: mail, password: password);
     String uid = result.user!.uid;
     DocumentSnapshot snapshot = await fireUser.doc(uid).get();
     return MyProfil(snapshot);
   }
 
-
   //Créer un utilisateur dans la base donnée
 
-  addUser(String uid , Map<String,dynamic> map){
+  addUser(String uid, Map<String, dynamic> map) {
     fireUser.doc(uid).set(map);
   }
 
-
   //Mettre à jour l'utilisateur
-  updateUser(String uid , Map<String,dynamic> map){
+  updateUser(String uid, Map<String, dynamic> map) {
     fireUser.doc(uid).update(map);
   }
 
-
-  deconnexion(){
+  deconnexion() {
     auth.signOut();
   }
 
-
   //Stocker image
-  Future <String> stockageImage(String nameImage,Uint8List data) async{
+  Future<String> stockageImage(String nameImage, Uint8List data) async {
     String urlChemin = "";
     //Stocker l'image
     TaskSnapshot download = await storage.ref("image/$nameImage").putData(data);
     //Récupper le lien de l'image
     urlChemin = await download.ref.getDownloadURL();
     return urlChemin;
-
   }
 
+  sendMessage(String texte, Utilisateur user, MyProfil moi) {
+    DateTime date = DateTime.now();
+    Map<String, dynamic> map = {
+      'from': moi.uid,
+      'to': user.uid,
+      'texte': texte,
+      'envoiMessage': date
+    };
+    String idDate = date.microsecondsSinceEpoch.toString();
+
+    addMessage(Map<String, dynamic> map, String uid) {
+      fire_message.doc(uid).set(map);
+    }
+
+    addConversation(Map<String, dynamic> map, String uid) {
+      fire_conversation.doc(uid).set(map);
+    }
+  }
+
+  Map<String, dynamic> getConversation(
+      String sender, Utilisateur partenaire, String texte, DateTime date) {
+    Map<String, dynamic> map = partenaire.toMap();
+    map['idmoi'] = sender;
+    map['lastmessage'] = texte;
+    map['envoimessage'] = date;
+    map['destinateur'] = partenaire.uid;
+
+    return map;
+  }
+
+  String getMessageRef(String from, String to, String date) {
+    String resultat = "";
+    List<String> liste = [from, to];
+    liste.sort((a, b) => a.compareTo(b));
+    for (var x in liste) {
+      resultat += x + "+";
+    }
+    resultat = resultat + date;
+    return resultat;
+  }
 }
---
+++ lib/model/Utilisateur.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Utilisateur {
+  late String _uid;
+  late String nom;
+  late String prenom;
+  String? avatar;
+  DateTime? naissance;
+  late String mail;
+
+  String get uid {
+    return _uid;
+  }
+
+  //Constructeur
+
+  MyProfil(DocumentSnapshot snapshot) {
+    _uid = snapshot.id;
+    Map<String, dynamic> map = snapshot.data() as Map<String, dynamic>;
+    nom = map["NOM"];
+    prenom = map["PRENOM"];
+    avatar = map["IMAGE"];
+    mail = map["MAIL"];
+    Timestamp? timpestamp = map["NAISSANCE"];
+    naissance = timpestamp?.toDate();
+  }
+
+//Méthodes
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map;
+    return map = {
+      'NOM': nom,
+      'PRENOM': prenom,
+      'IMAGE': avatar,
+      'MAIL': mail,
+      'NAISSANCE': naissance,
+    };
+  }
+}
