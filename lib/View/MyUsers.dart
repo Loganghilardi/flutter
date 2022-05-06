438f7dccbfb5a52cf820526f82ce0d47dffe1568
lgh <logan.ghilardi@netways.fr>
2022-05-06 12:09:41 +0200

    fix myusers avatar

--- lib/View/MyUsers.dart
+++ lib/View/MyUsers.dart
@@ -1,4 +1,3 @@
-
 import 'dart:io';
 import 'dart:typed_data';
 
@@ -10,129 +9,104 @@ import 'package:flutter/material.dart';
 import 'package:firstapplicationeisi/library/lib.dart';
 import 'package:file_picker/file_picker.dart';
 
-
-class MyUsers extends StatefulWidget{
+class MyUsers extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return MyUsersState();
   }
-
 }
 
-class MyUsersState extends State<MyUsers>{
+class MyUsersState extends State<MyUsers> {
   //Variables
   String? lienImage;
   Uint8List? bytesImages;
   String? nameImage;
 
-
-
-
   /////
-
-
-
 
   //Fonctions
   RecupererImage() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles(
-      withData: true,
-      type: FileType.image
-    );
-    if(result != null){
+    FilePickerResult? result = await FilePicker.platform
+        .pickFiles(withData: true, type: FileType.image);
+    if (result != null) {
       setState(() {
         nameImage = result.files.first.name;
         bytesImages = result.files.first.bytes;
       });
       popImage();
     }
-
   }
 
-  popImage(){
+  popImage() {
     showDialog(
         context: context,
         barrierDismissible: false,
-        builder: (context){
-          if(defaultTargetPlatform == TargetPlatform.iOS){
+        builder: (context) {
+          if (defaultTargetPlatform == TargetPlatform.iOS) {
             return CupertinoAlertDialog(
-              title: const Text("Souhaitez- vous avoir cette photo de profil?"),
-              content : Image.memory(bytesImages!),
-              actions : [
-                ElevatedButton(
-                    onPressed: (){
-                      Navigator.pop(context);
-                    },
-                    child: const Text("Annuler")
-                ),
-                ElevatedButton(
-                    onPressed: (){
-                      //Stocker mon image dans la base de donnée
-                      FirestoreHelper().stockageImage(nameImage!, bytesImages!).then((value){
-                        setState(() {
-                          lienImage = value;
-                          Myprofil.image = value;
-                          Map<String,dynamic> map ={
-                            "IMAGE": lienImage,
-                          };
-                          FirestoreHelper().updateUser(Myprofil.uid, map);
-                          Navigator.pop(context);
-
-
-
+                title:
+                    const Text("Souhaitez- vous avoir cette photo de profil?"),
+                content: Image.memory(bytesImages!),
+                actions: [
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text("Annuler")),
+                  ElevatedButton(
+                      onPressed: () {
+                        //Stocker mon image dans la base de donnée
+                        FirestoreHelper()
+                            .stockageImage(nameImage!, bytesImages!)
+                            .then((value) {
+                          setState(() {
+                            lienImage = value;
+                            Myprofil.image = value;
+                            Map<String, dynamic> map = {
+                              "IMAGE": lienImage,
+                            };
+                            FirestoreHelper().updateUser(Myprofil.uid, map);
+                            Navigator.pop(context);
+                          });
                         });
-                      });
-
-
-                    },
-                    child: const Text("Ok")
-                ),
-              ]
-            );
-          }
-          else
-            {
-              return AlertDialog(
-                  title: const Text("Souhaitez- vous avoir cette photo de profil?"),
-                  content : Image.memory(bytesImages!),
-                  actions : [
-                    ElevatedButton(
-                        onPressed: (){
-                          Navigator.pop(context);
-                        },
-                        child: const Text("Annuler")
-                    ),
-                    ElevatedButton(
-                        onPressed: (){
-                          //Stocker mon image dans la base de donnée
-                          FirestoreHelper().stockageImage(nameImage!, bytesImages!).then((value){
-                            setState(() {
-                              lienImage = value;
-                              Myprofil.image = value;
-                            });
-                              Map<String,dynamic> map ={
+                      },
+                      child: const Text("Ok")),
+                ]);
+          } else {
+            return AlertDialog(
+                title:
+                    const Text("Souhaitez- vous avoir cette photo de profil?"),
+                content: Image.memory(bytesImages!),
+                actions: [
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text("Annuler")),
+                  ElevatedButton(
+                      onPressed: () {
+                        //Stocker mon image dans la base de donnée
+                        FirestoreHelper()
+                            .stockageImage(nameImage!, bytesImages!)
+                            .then((value) {
+                          setState(() {
+                            lienImage = value;
+                            Myprofil.image = value;
+                          });
+                          Map<String, dynamic> map = {
                             "IMAGE": value,
                           };
                           FirestoreHelper().updateUser(Myprofil.uid, map);
-                          });
-                        
-                          Navigator.pop(context);
+                        });
 
-                        },
-                        child: const Text("Ok")
-                    ),
-                  ]
-
-              );
-            }
-
-        }
-    );
+                        Navigator.pop(context);
+                      },
+                      child: const Text("Ok")),
+                ]);
+          }
+        });
   }
-
-
-
 
   /////
   @override
@@ -142,23 +116,22 @@ class MyUsersState extends State<MyUsers>{
       padding: EdgeInsets.all(20),
       child: bodyPage(),
     );
-      
   }
 
-
-  Widget bodyPage(){
+  Widget bodyPage() {
     return Column(
       children: [
         //Logo de moi
         InkWell(
-          child: ImageRond(image: Myprofil.image,size: 150,),
-          onTap: (){
+          child: ImageRond(
+            image: Myprofil.image,
+            size: 150,
+          ),
+          onTap: () {
             print("J'ai appuyer sur l'image");
             RecupererImage();
           },
         ),
-
-
 
         //NOm et prénom
         Row(
@@ -167,34 +140,29 @@ class MyUsersState extends State<MyUsers>{
               width: MediaQuery.of(context).size.width - 100,
               child: TextField(
                 decoration: InputDecoration(
-                  hintText: Myprofil.prenom,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20)
-                  )
-                ),
-                onChanged: (newValue){
+                    hintText: Myprofil.prenom,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                onChanged: (newValue) {
                   setState(() {
                     Myprofil.prenom = newValue;
                   });
-
                 },
               ),
             ),
-
             IconButton(
-                onPressed: (){
-                  Map<String,dynamic> map ={
+                onPressed: () {
+                  Map<String, dynamic> map = {
                     "PRENOM": Myprofil.prenom,
                   };
                   FirestoreHelper().updateUser(Myprofil.uid, map);
-
                 },
-                icon: const Icon(Icons.edit)
-            ),
+                icon: const Icon(Icons.edit)),
           ],
         ),
-        const SizedBox(height: 10,),
-
+        const SizedBox(
+          height: 10,
+        ),
 
         Row(
           children: [
@@ -204,31 +172,27 @@ class MyUsersState extends State<MyUsers>{
                 decoration: InputDecoration(
                     hintText: Myprofil.nom,
                     border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20)
-                    )
-                ),
-                onChanged: (newValue){
+                        borderRadius: BorderRadius.circular(20))),
+                onChanged: (newValue) {
                   setState(() {
                     Myprofil.nom = newValue;
                   });
-
                 },
               ),
             ),
-
             IconButton(
-                onPressed: (){
-                  Map<String,dynamic> map ={
+                onPressed: () {
+                  Map<String, dynamic> map = {
                     "NOM": Myprofil.nom,
                   };
                   FirestoreHelper().updateUser(Myprofil.uid, map);
                 },
-                icon: const Icon(Icons.edit)
-            ),
+                icon: const Icon(Icons.edit)),
           ],
         ),
-        const SizedBox(height: 10,),
-
+        const SizedBox(
+          height: 10,
+        ),
 
         Text(Myprofil.mail)
 
@@ -236,5 +200,4 @@ class MyUsersState extends State<MyUsers>{
       ],
     );
   }
-
 }
